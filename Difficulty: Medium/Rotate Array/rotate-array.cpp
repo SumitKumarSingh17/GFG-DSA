51db3f0ca082f118b95ab23c8a3bd2bc1e8d4567
Sumit Kumar Singh <sumitkrsingh885@gmail.com>
2026-07-20 10:37:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate Array/rotate-array.cpp
+++ Difficulty: Medium/Rotate Array/rotate-array.cpp
@@ -1,20 +1,15 @@
 class Solution {
   public:
-    void reverseArray(vector<int>& arr, int start, int end) {
-        while (start < end) {
-            swap(arr[start], arr[end]);
-            start++;
-            end--;
-        }
-    }
     void rotateArr(vector<int>& arr, int d) {
         // code here
-        int n = arr.size();
-        if (n == 0) return;
-        d = d % n;
-        if (d == 0) return;
-        reverseArray(arr, 0, d - 1);
-        reverseArray(arr, d, n - 1);
-        reverseArray(arr, 0, n - 1);
+        int n=arr.size();
+        if(n==0) return;
+        d= d%n;
+        if(d==0) return;
+        vector<int> ans(n);
+        for(int i=0; i<n; i++){
+            ans[i]=arr[(i+d)%n];
+        }
+        arr=ans;
     }
 };
