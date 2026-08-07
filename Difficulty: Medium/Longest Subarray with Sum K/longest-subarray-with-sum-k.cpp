9c7a9cc396f8c88da143baed0b74e7bd2aadeefe
Sumit Kumar Singh <sumitkrsingh885@gmail.com>
2026-08-07 23:46:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
@@ -1,23 +1,21 @@
 class Solution {
   public:
     int longestSubarray(vector<int>& arr, int k) {
-        map<int, int> mpp;
+        unordered_map<int, int> mpp;
+        int maxi = 0;
         int sum = 0;
-        int maxlen = 0;
 
         for (int i = 0; i < arr.size(); i++) {
             sum += arr[i];
 
-            // If prefix sum itself equals k
-            if (sum == k)
-                maxlen = max(maxlen, i + 1);
+            // If subarray starts from index 0
+            if (sum == k) {
+                maxi = i + 1;
+            }
 
-            int rem = sum - k;
-
-            // If (sum-k) exists, update maximum length
-            if (mpp.find(rem) != mpp.end()) {
-                int len = i - mpp[rem];
-                maxlen = max(maxlen, len);
+            // If (sum - k) exists, we found a subarray with sum k
+            if (mpp.find(sum - k) != mpp.end()) {
+                maxi = max(maxi, i - mpp[sum - k]);
             }
 
             // Store first occurrence of prefix sum
@@ -26,6 +24,6 @@ class Solution {
             }
         }
 
-        return maxlen;
+        return maxi;
     }
 };
