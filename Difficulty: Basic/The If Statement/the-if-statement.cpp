026421c742a4c97fbcb7b48c706d6df84dd600f7
Sumit Kumar Singh <sumitkrsingh885@gmail.com>
2026-08-12 12:12:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The If Statement/the-if-statement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int n;
+    cin>>n;
+    if(n>100){
+        cout<<"Big"<<endl;
+        cout<<"Number";
+    }
+    else{
+        cout<<"Number";
+    }
+
+    return 0;
+}
