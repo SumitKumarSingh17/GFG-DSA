e510e5ce446175d7f518a5c8e9dc51c977b6f30f
Sumit Kumar Singh <sumitkrsingh885@gmail.com>
2026-09-02 12:04:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    string removeSpaces(string& s) {
+        // code here
+        string ans="";
+        for(char ch:s){
+            if(ch!=' '){
+                ans+=ch;
+            }
+        }
+        return ans;
+    }
+};
