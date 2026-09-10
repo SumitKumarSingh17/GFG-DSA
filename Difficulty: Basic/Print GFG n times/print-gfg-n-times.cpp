854cdf1b5d889f494d287a4182550054fce22d0b
Sumit Kumar Singh <sumitkrsingh885@gmail.com>
2026-09-10 23:49:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print GFG n times/print-gfg-n-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // code here
+    int i=0;
+    while(i<n){
+        cout<<"GFG" <<" ";
+        i++;
+    }
+
+    return 0;
+}
