77fd6dbdc91bc50349e6eb974d1e18fbb2d0fb17
Sumit Kumar Singh <sumitkrsingh885@gmail.com>
2026-09-20 23:19:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count Odd and Even/count-odd-and-even.cpp
+++ Difficulty: Basic/Count Odd and Even/count-odd-and-even.cpp
@@ -1,15 +1,11 @@
 class Solution {
   public:
-    /*Function to count even and odd elements in the array
-     * arr : Array with its elements
-     */
     pair<int, int> countOddEven(vector<int> &arr) {
-        // your code here
-        int cnto=0;
-        int cnte=0;
+        // code here
+        int cnte=0, cnto=0;
         for(int i=0; i<arr.size(); i++){
-            if(arr[i]%2!=0) cnto++;
-            else cnte++;
+            if(arr[i]%2==0) cnte++;
+            else cnto++;
         }
         return {cnto, cnte};
     }
